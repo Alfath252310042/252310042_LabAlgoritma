6b6ad3a5aa236ddef827ab7572c16a789316b6cd
Alfath252310042 <252310042@student.ibik.ac.id>
2025-10-31 06:33:32 +0700

    Add files via upload

---
+++ LAB_ALGORITMA DAN PEMROGRAMAN/PERTEMUAN 02/Latihan.cpp
@@ -0,0 +1,6 @@
+int	int angka = 10;	Menyimpan bilangan bulat (tanpa desimal).
+2	float	float suhu = 36.5;	Menyimpan bilangan desimal (presisi 6 digit).
+3	double	double phi = 3.14159265;	Menyimpan bilangan desimal dengan presisi lebih tinggi dari float.
+4	char	char huruf = 'A';	Menyimpan satu karakter.
+5	string	string nama = "Fath";	Menyimpan kumpulan karakter (teks).
+6	bool	bool status = true;	Menyimpan nilai logika: true atau false.
